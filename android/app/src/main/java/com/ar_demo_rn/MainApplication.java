80c0b30dc4d653d43d4956623bbb50d8ea76e8b4
Viraj Patel <mspviraj@hotmail.com>
2021-11-30 15:00:38 +0530

    Android Setup with Viro SDK

--- android/app/src/main/java/com/ar_demo_rn/MainApplication.java
+++ android/app/src/main/java/com/ar_demo_rn/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.viromedia.bridge.ReactViroPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,7 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          return packages;
+
+            packages.add(new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf("AR")));
+
+            return packages;
         }
 
         @Override
